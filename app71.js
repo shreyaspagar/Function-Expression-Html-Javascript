a6e3ba92b35a567481fd7edcf8fbccb25148dd3b
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-08 13:20:58 +0530

    Add files via upload

---
+++ app71.js
@@ -0,0 +1,14 @@
+let name = "Shreyas";
+let x = 5;
+
+let sum = function(a,b) {
+    return  a+b;
+}
+
+let hello = function() {
+   console.log("hello"); 
+}
+
+hello = function() {
+    console.log("Namaste");
+}
